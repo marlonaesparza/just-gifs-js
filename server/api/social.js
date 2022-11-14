af269aef94713bfd4522956b2eaa283c0dae783a
marlonaesparza <marlonaesparza@gmail.com>
2022-11-13 22:44:37 -0800

    Social features complete

--- server/api/social.js
+++ server/api/social.js
@@ -3,31 +3,23 @@ const SocialBusiness = require('./business/social');
 
 
 router.post('/createConnection', (req, res) => {
-  //
+  return SocialBusiness.createConnection(req, res);
 });
 
 router.delete('/deleteConnection', (req, res) => {
-  //
-});
-
-router.get('/allUserConnections', (req, res) => {
-  //
+  return SocialBusiness.deleteConnection(req, res);
 });
 
 router.get('/allPotentialConnections', (req, res) => {
-  SocialBusiness.getAllPotentialConnections(req, res);
+  return SocialBusiness.getAllPotentialConnections(req, res);
 });
 
 router.post('/createRequest', (req, res) => {
-  SocialBusiness.createRequest(req, res);
+  return SocialBusiness.createRequest(req, res);
 });
 
 router.delete('/deleteRequest', (req, res) => {
-  //
-});
-
-router.get('/allRequests', (req, res) => {
-  //
+  return SocialBusiness.deleteRequest(req, res);
 });
 
 
