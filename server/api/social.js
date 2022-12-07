328da382682e296d1d52f0874677a891c07d9361
marlonaesparza <marlonaesparza@gmail.com>
2022-12-06 20:21:44 -0800

    Updated: Social.js (API router)
    Updates:
    - Introduced new routes: "/searchForPotentialConnections", and
      "/searchForUserConnections".
    - Forwards requests to business functions.

--- server/api/social.js
+++ server/api/social.js
@@ -2,6 +2,19 @@ const router = require('express').Router();
 const SocialBusiness = require('./business/social');
 
 
+router.get('/allPotentialConnections', (req, res) => {
+  return SocialBusiness.getAllPotentialConnections(req, res);
+});
+
+
+router.get('/searchForPotentialConnections', (req, res) => {
+  return SocialBusiness.searchForPotentialConnections(req, res);
+});
+
+router.get('/searchForUserConnections', (req, res) => {
+  return SocialBusiness.searchForUserConnections(req, res);
+});
+
 router.post('/createConnection', (req, res) => {
   return SocialBusiness.createConnection(req, res);
 });
@@ -10,9 +23,6 @@ router.delete('/deleteConnection', (req, res) => {
   return SocialBusiness.deleteConnection(req, res);
 });
 
-router.get('/allPotentialConnections', (req, res) => {
-  return SocialBusiness.getAllPotentialConnections(req, res);
-});
 
 router.post('/createRequest', (req, res) => {
   return SocialBusiness.createRequest(req, res);
